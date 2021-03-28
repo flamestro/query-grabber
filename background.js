7e656d378d1975aa6cab8a0cfee30157cfde03d6
flamestro <NoReplyFlamestro@gmail.com>
2021-03-28 20:32:52 +0200

    Finish main functionality

--- background.js
+++ background.js
@@ -1,6 +1,52 @@
-let color = '#3aa757';
+let pathParameter = '';
+
+const contextMenuItem = {
+    "id": "pathParamGrabber",
+    "title": "Grab Parameter",
+    "contexts": ["selection", "link"],
+}
+
+// https://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-get-parameters
+function getQueryParams(url) {
+    url = url.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(url)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+//https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension
+function copyToClipboard(textToClip) {
+    let copyFrom = document.createElement("textarea");
+    copyFrom.textContent = textToClip;
+    document.body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.blur();
+    document.body.removeChild(copyFrom);
+}
+
+const clickHandler = (event) => {
+    if (event.menuItemId === contextMenuItem.id && event.linkUrl) {
+        chrome.storage.sync.get("pathParameter", ({pathParameter}) => {
+            console.log(`${pathParameter} = ${getQueryParams(event.linkUrl)[pathParameter]}`)
+            copyToClipboard(getQueryParams(event.linkUrl)[pathParameter])
+        });
+    }
+}
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color });
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    console.log("init")
+    chrome.storage.sync.set({pathParameter});
+
+    chrome.contextMenus.create(contextMenuItem);
+
+    chrome.contextMenus.onClicked.addListener(clickHandler);
+    console.log("done")
 });
---
+++ popup.js
@@ -0,0 +1,10 @@
+let pathParameterInput = document.getElementById("pathParam");
+
+pathParameterInput.addEventListener("keyup", () => {
+    let pathParameter = pathParameterInput.value
+    chrome.storage.sync.set({ pathParameter });
+})
+
+chrome.storage.sync.get("pathParameter", ({pathParameter}) => {
+    pathParameterInput.placeholder = pathParameter ? `current: ${pathParameter}`  : "Enter Parameter";
+});
