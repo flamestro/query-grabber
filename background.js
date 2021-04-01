ffbacabe6f78d5a67e900ea9920fd2814ae9d29e
flamestro <NoReplyFlamestro@gmail.com>
2021-04-01 17:01:18 +0200

    Add notification feedback for the clip event

--- background.js
+++ background.js
@@ -30,11 +30,49 @@ const copyToClipboard = (textToClip) => {
     document.body.removeChild(copyFrom);
 }
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+const clearNotification = (notificationId) => {
+    chrome.notifications.clear(
+        notificationId,
+        () => {}
+    )
+}
+
+const notifyUserWithText = (text) => {
+    let notificationId = uuidv4();
+    chrome.notifications.create(
+        notificationId,
+        {
+            type: "basic",
+            iconUrl: "/assets/clipboard128.png",
+            title: "Path Grabber",
+            message: text
+        },
+        () => {
+            setTimeout(() => {
+                clearNotification(notificationId);
+            }, 1500);
+        }
+    );
+}
+
 const clickHandler = (event) => {
     if (event.menuItemId === contextMenuItem.id && event.linkUrl) {
         chrome.storage.sync.get("pathParameter", ({pathParameter}) => {
-            console.log(`${pathParameter} = ${getQueryParams(event.linkUrl)[pathParameter]}`)
-            copyToClipboard(getQueryParams(event.linkUrl)[pathParameter])
+            if (getQueryParams(event.linkUrl)[pathParameter]) {
+                console.log(`${pathParameter} = ${getQueryParams(event.linkUrl)[pathParameter]}`)
+                copyToClipboard(getQueryParams(event.linkUrl)[pathParameter])
+                notifyUserWithText(`Copied ${pathParameter}`);
+            } else {
+                notifyUserWithText(`No ${pathParameter} in link`);
+            }
         });
     }
 }
