e59c0645615606d79c094a1ef1c0883c58719be3
flamestro <NoReplyFlamestro@gmail.com>
2021-04-01 17:23:20 +0200

    add comment with reference to code example

--- background.js
+++ background.js
@@ -30,6 +30,7 @@ const copyToClipboard = (textToClip) => {
     document.body.removeChild(copyFrom);
 }
 
+//https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
